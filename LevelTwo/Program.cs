95298deced2565dd0f2d1e3c791f8dd8833ca479
Arthur Santos <arthurluiz.developer@gmail.com>
2025-03-21 13:27:55 +0000

    Exercicio 2 resolvido

---
+++ LevelTwo/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Spectre.Console;
+
+namespace LevelTwo
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            CanvasImage image = new CanvasImage("tux.jpg"); 
+
+            if (args.Length == 0)
+            {
+                image.MaxWidth(24);
+            }
+            else
+            {
+                image = new CanvasImage(args[0]);
+
+                image.MaxWidth(int.Parse(args[1]));
+            }
+
+
+            AnsiConsole.Write(image);
+        }
+    }
+}
