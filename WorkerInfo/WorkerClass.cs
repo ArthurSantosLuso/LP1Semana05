48f20e12b06d4929d5bb173bcdac6185d9308ca2
Arthur Santos <arthurluiz.developer@gmail.com>
2025-03-21 14:55:04 +0000

    Exericio da aula pratica realizado

---
+++ WorkerInfo/WorkerClass.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace WorkerInfo
+{
+    public class WorkerClass
+    {
+        public WorkerClass()
+        {
+            Id = 0;
+            Name = "";
+            Job = "";
+        }
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Job { get; set; }
+    }
+}
---
+++ WorkerTable/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Reflection.Metadata.Ecma335;
+using Bogus;
+using Spectre.Console;
+using WorkerInfo;
+
+namespace WorkerTable
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Randomizer.Seed = new Random(int.Parse(args[0]));
+            Table tabela = new Table();
+            tabela.AddColumn("ID");
+            tabela.AddColumn("Name");
+            tabela.AddColumn("Job");
+
+            Faker fakerInfo = new Faker("pt_PT");
+
+            WorkerClass[] workers = new WorkerClass[(int.Parse(args[0]))];
+
+            for (int i = 0; i < int.Parse(args[0]); i++)
+            {
+                tabela.AddRow(fakerInfo.Random.Number(0, 1000).ToString(), fakerInfo.Name.FullName(), fakerInfo.Name.JobTitle());
+            }
+
+            AnsiConsole.Write(tabela);
+
+
+
+        }
+    }
+}
